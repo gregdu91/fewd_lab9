5599c30652264519358b13167f7f565bcef70032
gregdu91 <55101978+gregdu91@users.noreply.github.com>
2020-11-24 18:25:36 +0100

    2

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bulma/css/bulma.css';
+import Jul from './img/jul.jpg'
+  
+
+class ClickImg extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+          clickNum: 0
+      };
+      this.handleClicks = this.handleClicks.bind(this);
+  }
+  handleClicks() {
+      this.setState(prevState => ({
+          clickNum: prevState.clickNum + 1
+      }));
+
+  }
+
+
+
+render() {
+  return (
+      <div>
+          <h1>The image is clicked {this.state.clickNum} times.</h1>
+          <img onClick={this.handleClicks} src={Jul}></img>
+      </div>
+    );
+  }
+};
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
-  document.getElementById('root'),
+  <ClickImg />,
+  document.getElementById('root')
+
 );
 
-ReactDOM.render(
-  <h1>Goodbye</h1>,
-  document.getElementById('other'),
-);
