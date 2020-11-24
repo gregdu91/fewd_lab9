9c3369f5746992493e47dae4f08d18ecd5296223
gregdu91 <55101978+gregdu91@users.noreply.github.com>
2020-11-24 18:22:36 +0100

    1

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello World</h1>,
+  document.getElementById('root'),
+);
+
+ReactDOM.render(
+  <h1>Goodbye</h1>,
+  document.getElementById('other'),
+);
